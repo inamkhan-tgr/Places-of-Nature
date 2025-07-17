8210ce53ffe533ef0bcfa9692ad756fd56ef2cc4
inamkhan-tgr <inamsamad986@gmail.com>
2025-07-17 20:04:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const panels = document.querySelectorAll(".panel");
+console.log(panels)
+
+for (let panel of panels){
+    panel.addEventListener("click", () =>  {
+        removeActiveClass();
+        panel.classList.add("active")
+    })
+}
+
+let removeActiveClass = (panel) =>{
+    for (panel of panels){
+        panel.classList.remove("active")
+    }
+}
